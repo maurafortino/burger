3daa6c17c32f6fcc41cde0139168c79c4868eed3
maurafortino <maurafortino@gmail.com>
2019-11-10 17:00:49 -0500

    initial file rendering

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Eggers Can't Be Cheesers");
+INSERT INTO burgers (burger_name) VALUES ("Poblano Picasso");
+INSERT INTO burgers (burger_name) VALUES ("Beets of Burden");
+INSERT INTO burgers (burger_name) VALUES ("The Human Polenta-pede");
+INSERT INTO burgers (burger_name) VALUES ("Curd-fect Strangers");
+INSERT INTO burgers (burger_name) VALUES ("Fig-eta Bout It");
