8e145a6c8c7ce080d13188dacd421e3c3b8d5bd6
maurafortino <maurafortino@gmail.com>
2019-11-11 18:24:45 -0500

    edited connection file

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,11 @@ var mysql = require("mysql");
 
 var connection;
 
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -15,13 +16,13 @@ if (process.env.JAWSDB_URL) {
 };
 
 // Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
