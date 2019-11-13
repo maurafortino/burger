740a6cfd8da081c4b9eaae81cc151119f4f5cd94
maurafortino <maurafortino@gmail.com>
2019-11-12 23:06:48 -0500

    edited the styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,4 @@ INSERT INTO burgers (burger_name) VALUES ("Poblano Picasso");
 INSERT INTO burgers (burger_name) VALUES ("Beets of Burden");
 INSERT INTO burgers (burger_name) VALUES ("The Human Polenta-pede");
 INSERT INTO burgers (burger_name) VALUES ("Curd-fect Strangers");
-INSERT INTO burgers (burger_name) VALUES ("Fig-eta Bout It");
+INSERT INTO burgers (burger_name) VALUES ("Fig-eta Bout It");heroku
